2a1506559703f4aafee8af3ef02a0bb830b9796c
OstKost <ostkost@list.ru>
2018-12-17 10:43:03 +0300

    setting up server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const mongoURI = 'localhost:20017'
+
+mongoose
+	.connect(
+		mongoURI,
+		{ useNewUrlParser: true }
+	)
+	.then(() => console.log('MongoDB connected!'))
+	.catch(error => console.error(error))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use('/api/', routes)
+
+module.exports = app
