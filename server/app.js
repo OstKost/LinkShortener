62e5624499fa086a5ec9af166f4b3ecceb824bb2
OstKost <ostkost@list.ru>
2018-12-17 17:36:56 +0300

    init client add logging for http requests

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const morgan = require('morgan')('dev')
 const routes = require('./routes')
 
 const mongoURI = 'mongodb://localhost:27017/shorturls'
@@ -14,6 +15,7 @@ mongoose
 	.then(() => console.log('MongoDB connected!'))
 	.catch(error => console.error(error))
 
+app.use(morgan)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
