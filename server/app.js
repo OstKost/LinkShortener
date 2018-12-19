5b3552b30e0dcc5f95df7273096cc9ef5419d91d
OstKost <ostkost@list.ru>
2018-12-19 11:41:53 +0300

    added production environment

--- server/app.js
+++ server/app.js
@@ -21,4 +21,11 @@ app.use(bodyParser.json())
 
 app.use('/', routes)
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('../client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 module.exports = app
