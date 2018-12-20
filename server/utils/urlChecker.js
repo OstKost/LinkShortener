02a3c75e95fb12fef5ba67a788d506432c891441
OstKost <ostkost@list.ru>
2018-12-20 17:49:31 +0300

    refactor urlCheker

--- server/utils/urlChecker.js
+++ server/utils/urlChecker.js
@@ -2,31 +2,13 @@ const axios = require('axios')
 const _url = require('url')
 
 async function checkUrl(fullUrl) {
-	const { protocol, href } = { ..._url.parse(fullUrl) }
-	const baseUrl = href.slice(-1) === '/' ? href.slice(0, -1) : href
-	let url = ''
-
-	// если в адресе есть http или https
-	if (protocol) {
-		url = baseUrl
-		try {
-			if (await axios.head(url)) return url
-		} catch (error) {
-			console.error(error.message)
-		}
-	}
-
-	// если https или http нет в адресе
-	try {
-		url = `https://${baseUrl}`
-		if (await axios.head(url)) return url
-	} catch (error) {
-		console.error(error.message)
-	}
+	const hasProtocol = /^(http:\/\/|https:\/\/)./.test(fullUrl)
+	const url = (hasProtocol ? '' : 'https://') + fullUrl
+	const href = _url.parse(url).href
 
 	try {
-		url = `http://${baseUrl}`
-		if (await axios.head(url)) return url
+		if (await axios.head(href)) return href
+		if (await axios.head(href.replace('https', 'http'))) return href
 	} catch (error) {
 		console.error(error.message)
 	}
